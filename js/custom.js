17158ccce105aaa1b95e2dd4f8e0bee64f55900e
adexot <adekunleoseni47@gmail.com>
2017-10-26 15:10:46 +0100

    completed the website design

--- js/custom.js
+++ js/custom.js
@@ -11,5 +11,26 @@ $(document).ready(function(){
     $('.customers').mouseout(function(){
         $('.customer-button').hide();
     });
-    
+
+    // URL style
+    var url = document.location.href.split('/');
+    var pattern = stripUrl(url);   
+    $('nav div ul li a').each(function(){
+        var refLocation = $(this).attr('data-text');
+        //if the ref location has an s affter it
+        var refLocations = $(this).attr('data-text') + 's';
+        var v = pattern.test(refLocation);
+        var u = pattern.test(refLocations);
+        if('/' + pattern ==  refLocation  || v || u){
+            $(this).addClass('active');
+        }
+    });
+
+    function stripUrl(url){
+        var urlLength = url.length;
+        var fragment = url[urlLength-1];
+        var key = fragment.split('.');
+        var pattern = new RegExp(key[0]);
+        return pattern;
+    }
 });
